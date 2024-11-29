c1258a818785cbef6b85087b7405e51441941deb
kaju501 <riyansaikia97@gmail.com>
2024-11-29 12:27:41 +0530

    update

--- src/components/home/RightDoor.tsx
+++ src/components/home/RightDoor.tsx
@@ -1,29 +1,46 @@
 import React from 'react';
 import { Box } from '../command/Box';
 import { RightDoorProps } from "../global/type";
+import { useTimeManager } from '../hooks/useTimeManager';
+
 
 const RightDoor: React.FC<RightDoorProps> = ({ isEntered }) => {
+
+    const initialTime = 600;
+
+    const { timeLeft, isTimeExhausted } = useTimeManager(initialTime);
+
+    const hoursLeft = Math.floor(timeLeft / 3600);
+    const minutesLeft = Math.floor((timeLeft % 3600) / 60);
+    const secondsLeft = timeLeft % 60;
+
+
+    const formattedHours = hoursLeft > 9 ? hoursLeft : `0${hoursLeft}`;
+    const formattedMinutes = minutesLeft > 9 ? minutesLeft : `0${minutesLeft}`;
+    const formattedSeconds = secondsLeft > 9 ? secondsLeft : `0${secondsLeft}`;
+
     return (
         <div
             className={`min-h-screen ${isEntered ? 'w-0 p-0 opacity-0' : 'w-[50%] p-20'} bg-black border-primary border-[6px] transition-all duration-1000`}
         >
             <div className="flex flex-col items-end justify-start pt-28 pr-10">
-                <Box width="w-28" height="h-8" borderOnly />
-
-                <div className="py-2">
-                    <Box width="w-28" height="h-4" borderOnly />
-                    <Box width="w-28" height="h-4" isPrimary />
-                </div>
 
                 <div className="space-y-2">
-                    {[...Array(5)].map((_, i) => (
+                    {[...Array(7)].map((_, i) => (
                         <Box key={i} width="w-28" height="h-8" isPrimary />
                     ))}
                 </div>
 
                 <div className="mt-8 text-right">
-                    <p className="text-4xl font-bold text-primary">45 hours left</p>
-                    <p className="text-sm text-gray-400">out of 50 hours</p>
+                    {isTimeExhausted ? (
+                        <p className="text-4xl font-bold text-primary">00:00:00</p>
+                    ) : (
+                        <p className="text-4xl font-bold text-primary">
+
+                            {formattedHours}:{formattedMinutes}:{formattedSeconds} min left
+                        </p>
+                    )}
+                    <p className="text-sm text-gray-400">out of 10 min</p>
                 </div>
             </div>
         </div>
